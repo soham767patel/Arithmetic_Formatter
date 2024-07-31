9881afb545c2ac6a0da24a98e1c08500aff88c12
Soham Patel <sohampat@umich.edu>
2024-07-31 17:09:56 -0400

    Added limit of 5 problems

--- main.py
+++ main.py
@@ -5,6 +5,11 @@
 # ----             ----
 #                   287
 def arithmetic_arranger(problems, show_answers = False):
+    #The limit of how many problem can be submitted as once will be 5
+    if problems.len() > 5:
+        pass
+    else:
+        ValueError("Error: Too many problems.")
     return problems
 
 print(f'\n{arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"])}')
