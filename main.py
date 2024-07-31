4afcb33dee5d71ac4b2aebd8708ef04bd161fef0
Soham Patel <sohampat@umich.edu>
2024-07-31 17:06:12 -0400

    Basic descripton

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+#this formatter should convert "235 + 52" to if show_answers is false
+#and it should add the answer if show_answer is true
+#  235              235
+# + 52      or     + 52
+# ----             ----
+#                   287
 def arithmetic_arranger(problems, show_answers = False):
     return problems
 
