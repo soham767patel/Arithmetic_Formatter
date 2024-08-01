c2097e43e891cf0e71f313cbae719162bb14f488
Soham Patel <sohampat@umich.edu>
2024-08-01 13:30:31 -0400

    Created some changes

--- main.py
+++ main.py
@@ -6,13 +6,19 @@
 #                   287
 def arithmetic_arranger(problems, show_answers = False):
     #The limit of how many problem can be submitted as once will be 5
-    solved_problems = []
+    solved = ''
+    solution
+    dashes = ''
+    firstline = []
+    secondline = ['\n']
+    thirdline = ['\n']
+    fourthline = ['\n']
     if len(problems) < 5:
         for problem in problems:
-            val = 0 #number of additon or subtraction signs found
+            val = 0
             num1 = ''
             num2 = ''
-            operand = ''
+            operator = ''
             for char in problem:
                 if char == ' ':
                     val+=1
@@ -20,51 +26,48 @@ def arithmetic_arranger(problems, show_answers = False):
                     if val == 0:
                         num1+=char
                     elif val == 1:
-                        operand = char
+                        operator = char
                     elif val == 2:
                         num2+=char
-            #if either number is greater than four digits throw an error
             if (len(num1) > 4 | len(num2) > 4):
                 ValueError("Error: Numbers cannot be more than four digits.")
             elif not num1.isnumeric():
                 ValueError("Error: Numbers must only contain digits.")
             elif not num2.isnumeric():
                 ValueError("Error: Numbers must only contain digits.")
-            elif not (operand == '+' or operand == '-'):
+            elif not (operator == '+' or operator == '-'):
                 ValueError("Error: Operator must be '+' or '-'.")
-            #the problem is valid
-            if show_answers:
-               max_len = max(len(num1), len(num1))
-               if len(num1) < max_len:
-                    for _ in range(max_len-len(num1)):
-                        num1 = ' ' + num1
-               elif len(num2) < max_len:
-                    for _ in range(max_len-len(num2)):
-                        num2 = ' ' + num2
-               curr_problem = f'{num1}\n{operand} {num2}\n'
-               solved_problems.append(curr_problem)
-               dash = ''
-               for _ in range(max_len+1):
-                    dash = '-' + dash 
-               curr_problem += dash
-               solved_problems.append(curr_problem)
-            else:
-                
-                max_len = max(len(num1), len(num2))
-                if  len(num1) < max_len:
-                    for _ in range(max_len-len(num1)):
-                        num1 = ' ' + num1
-                elif len(num2) < max_len:
-                    for _ in range(max_len-len(num2)):
-                        num2 = ' ' + num2
-                curr_problem = f'{num1}\n{operand} {num2}\n'
-                dash = ''
-                for _ in range(max_len+1):
-                    dash = '-' + dash 
-                curr_problem += dash
-                solved_problems.append(curr_problem)
+            maxdifference = abs(len(num1)-len(num2))
+            if operator == '+':
+                solution = int(num1) + int(num2)
+            elif operator == '-':
+                solution = int(num1) - int(num2)
+            if len(num1) > len(num2):
+                for _ in maxdifference:
+                    num2 = ' ' + num2
+                for _ in len(num1) + 2:
+                    dashes = '-' + dashes 
+                for _ in len(num1) + 2 - len(solution):
+                    solution = ' ' + solution
+            elif len(num2) > len(num1):
+                for _ in maxdifference:
+                    num1 = ' ' + num1
+                for _ in len(num2) + 2:
+                    dashes = '-' + dashes 
+            firstline.append('  ' + num1)
+            secondline.append(operator + ' ' + num2)
+            thirdline.append(dashes)
+            fourthline.append(solution)
+
+
     else:
         ValueError("Error: Too many problems.")
-    return solved_problems
+    if show_answers:
+        solved = '    '.join(firstline) + '    '.join(secondline) + '    '.join(thirdline) + '    '.join(fourthline)
+        return solved
+    else:
+        solved = '    '.join(firstline) + '    '.join(secondline) + '    '.join(thirdline)
+        return solved
+
 
 print(arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"]))
