52eb7e23bf36bb5b0efe1b5709c569c9dbe05cdc
Soham Patel <sohampat@umich.edu>
2024-08-01 00:19:50 -0400

    Created a wrongly working function

--- main.py
+++ main.py
@@ -6,10 +6,65 @@
 #                   287
 def arithmetic_arranger(problems, show_answers = False):
     #The limit of how many problem can be submitted as once will be 5
-    if problems.len() > 5:
-        pass
+    solved_problems = []
+    if len(problems) < 5:
+        for problem in problems:
+            val = 0 #number of additon or subtraction signs found
+            num1 = ''
+            num2 = ''
+            operand = ''
+            for char in problem:
+                if char == ' ':
+                    val+=1
+                else:
+                    if val == 0:
+                        num1+=char
+                    elif val == 1:
+                        operand = char
+                    elif val == 2:
+                        num2+=char
+            #if either number is greater than four digits throw an error
+            if (len(num1) > 4 | len(num2) > 4):
+                ValueError("Error: Numbers cannot be more than four digits.")
+            elif not num1.isnumeric():
+                ValueError("Error: Numbers must only contain digits.")
+            elif not num2.isnumeric():
+                ValueError("Error: Numbers must only contain digits.")
+            elif not (operand == '+' or operand == '-'):
+                ValueError("Error: Operator must be '+' or '-'.")
+            #the problem is valid
+            if show_answers:
+               max_len = max(len(num1), len(num1))
+               if len(num1) < max_len:
+                    for _ in range(max_len-len(num1)):
+                        num1 = ' ' + num1
+               elif len(num2) < max_len:
+                    for _ in range(max_len-len(num2)):
+                        num2 = ' ' + num2
+               curr_problem = f'{num1}\n{operand} {num2}\n'
+               solved_problems.append(curr_problem)
+               dash = ''
+               for _ in range(max_len+1):
+                    dash = '-' + dash 
+               curr_problem += dash
+               solved_problems.append(curr_problem)
+            else:
+                
+                max_len = max(len(num1), len(num2))
+                if  len(num1) < max_len:
+                    for _ in range(max_len-len(num1)):
+                        num1 = ' ' + num1
+                elif len(num2) < max_len:
+                    for _ in range(max_len-len(num2)):
+                        num2 = ' ' + num2
+                curr_problem = f'{num1}\n{operand} {num2}\n'
+                dash = ''
+                for _ in range(max_len+1):
+                    dash = '-' + dash 
+                curr_problem += dash
+                solved_problems.append(curr_problem)
     else:
         ValueError("Error: Too many problems.")
-    return problems
+    return solved_problems
 
-print(f'\n{arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"])}')
+print(arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"]))
